28c983a7e3e99b524fecf016965fecc4a0de89d9
Reda-F <4071019@alu.murciaeduca.es>
2025-01-22 13:35:43 +0100

    Delete Buenas directory

--- Buenas/Scripts/instrucciones.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-var scn_charged = load ("res://Scenes/menu_principal.tscn")
-
-
-func _on_button_pressed():
-	$"/root/Global/AudioStreamPlayer2D".play()
-	get_tree().change_scene_to_packed(scn_charged)
-	pass # Replace with function body.
